c7df9e027f444a2e24705b17632b4d47f917f041
Willifme <me@williamcollier.me.uk>
2017-09-13 09:27:22 +0100

    Expand repl

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,21 @@
-fn main() {}
+extern crate rustyline;
+extern crate robin_core;
+
+use rustyline::Editor;
+use robin_core::parser::number;
+
+fn main() {
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        let readline = rl.readline(">>> ");
+
+        match readline {
+            Ok(line) => {
+                println!("{:?}", number::numeric_literals(line.as_bytes()));
+            },
+
+            Err(_) => break, 
+        }
+    }
+}
