e7ef40ab2eecd6804c6b4dbc7dd6dfeb657cd8cd
Willifme <me@williamcollier.me.uk>
2018-02-11 14:23:15 +0000

    Push to Desktop Computer

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
 extern crate rustyline;
 extern crate robin_core;
 
+use std::process::{Command, Child, Stdio};
+use std::io::{Read, Write};
+
 use rustyline::Editor;
 use robin_core::parser;
-use robin_core::error;
-use robin_core::to_javascript::ToJavaScript;
+use robin_core::compiler::Compiler;
 
 struct Repl {
     editor: Editor<()>,
-    error_stack: error::ErrorStack,
+    compiler: Compiler,
+    node: Child,
 }
 
 impl Repl {
     fn new() -> Repl {
         Repl {
             editor: Editor::<()>::new(),
-            error_stack: error::ErrorStack(vec![]),
+            compiler: Compiler::new(),
+            node: Command::new("node") 
+                        .stdin(Stdio::piped())
+                        .stdout(Stdio::piped())
+
+                        // Use the --interactive flag to start the REPL despite stdin not
+                        // being a terminal see node --help for more
+                        .args(&["-i"])
+                        .spawn()
+                        .expect("Node.js not installed or found in path, please fix this!")
         }
     }
 
     fn repl(&mut self) {
+        println!("Welcome to Robin! Type code in below.");
+
+        println!("When you run code in the REPL, you might find some Robin and some Node.js errors! Be careful");
+
         loop {
             let readline = self.editor.readline(">>> ");
 
@@ -35,21 +51,54 @@ impl Repl {
         }
     }
 
+    fn handle_node_repl(&mut self, input: String) {
+        if let Some(ref mut stdin) = self.node.stdin.as_mut() {
+            write!(stdin, "{}\n", input);
+
+            stdin.flush();
+        }
+
+        self.node.wait().expect("Failed to read stdout");
+
+        let mut buffer = String::new();
+
+        if let Some(ref mut stdout) = self.node.stdout {
+            stdout.read_to_string(&mut buffer).unwrap();
+
+            println!("{}", buffer);
+        }
+    }
+
     fn parse(&mut self, line: &str) {
         match parser::parse(line) {
-            Ok(expr) => { println!("{:?}", expr); println!("{}", expr.eval())},
-            Err(e) => {
-                // TODO: Remove this unwrap
-                self.error_stack
-                    .0
-                    .push(error::Error((error::ErrorLevel::Error, e)));
+            // TODO: Remove this unwrap
+            Ok(expr) => { 
+                let output = self.compiler.compile(&[expr]);
 
-                // TODO: Remove unwrap
-                println!("{}", self.error_stack.0.last().unwrap());
+                // TODO: Handle error stack here
+                if !self.compiler.errors.0.is_empty() {
+                    self.compiler.errors.0
+                        .iter()
+                        .for_each(|e| println!("{}", e));
+
+                    // Clear the error stack on each input
+                    self.compiler.errors.0.clear();
+
+                } else {
+                    println!("JS Code: {}", output);
+
+                    self.handle_node_repl(output);
+                }
+            },
+
+            Err(e) => {
+                // TODO: Handle this error
+                println!("Error");
             }
         }
     }
 }
+
 fn main() {
     let mut repl = Repl::new();
 
